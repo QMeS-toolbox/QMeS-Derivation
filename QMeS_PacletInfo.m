029037ada07441ee54da1076665ca280da46334d
CoralieSchneider <74408587+CoralieSchneider@users.noreply.github.com>
2020-12-08 12:18:37 +0100

    Add files via upload

--- QMeS_PacletInfo.m
+++ QMeS_PacletInfo.m
@@ -2,12 +2,12 @@
 
 (* Paclet Info File *)
 
-(* created 05/08/2020*)
+(* created 08/12/2020*)
 
 Paclet[
     Name -> "QMeS",
-    Version -> "0.2",
-    MathematicaVersion -> "11+",
+    Version -> "1.0",
+    MathematicaVersion -> "12+",
     Description -> "We present the mathematica package QMeS. It derives symbolic flow equations from a master equation (FRG, mSTI, DSE) by using a superfield formalism. Explicitly it's modules allow to derive DSEs, take functional derivatives, trace over field space and do a momentum routing for 1-loop diagrams while keeping track of prefactors and signs that arise from fermionic commutation relations.",
     Creator -> "Jan M. Pawlowski, Coralie S. Schneider, Nicolas Wink",
     Extensions -> 
