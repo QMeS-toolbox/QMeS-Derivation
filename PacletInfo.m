569b66ddf6b4452b90ba48f51b00df1df2affcd3
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2024-08-08 15:52:05 +0200

    fix: partial fix of broken paclet info

---
+++ PacletInfo.m
@@ -0,0 +1,24 @@
+(* ::Package:: *)
+
+PacletObject[
+    <|
+        "Name" -> "QMeS",
+        "Version" -> "1.2",
+        "WolframVersion" -> "12+",
+    	"Description" -> "We present the mathematica package QMeS. It derives symbolic flow equations from a master equation (FRG, mSTI, DSE) by using a superfield formalism. Explicitly it's modules allow to derive DSEs, take functional derivatives, trace over field space and do a momentum routing for 1-loop diagrams while keeping track of prefactors and signs that arise from fermionic commutation relations.",
+    	"Creator" -> "Jan M. Pawlowski, Coralie S. Schneider, Nicolas Wink",
+        "Extensions" ->
+            {
+                {
+                    "Kernel",
+                    "Context" -> {
+                        { "QMeSderivation`" , "QMeSderivation.m" }
+                    }
+                },
+                {
+                    "Documentation",
+                    "Language" -> "English"
+                }
+            }
+    |>
+]
