fcac557a9b88ddab33226151e7955b38e7355fd9
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2024-08-08 16:05:31 +0200

    feat: small script to create the zip files used by the installer

---
+++ make_zip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p QMeSderivation
+cp -r example QMeSderivation
+cp QMeSderivation.m QMeSderivation
+cp PacletInfo.m QMeSderivation
+cp LICENSE QMeSderivation
+cp ReleaseNotes.md QMeSderivation
+cp README.md QMeSderivation
+
+rm ./QMeSderivation.zip
+zip -r QMeSderivation.zip QMeSderivation
+
+rm -rf ./QMeSderivation/
